ec28435532a7b90145e1aca1b8316d5f353a0563
jawuermkr <guitarro92@hotmail.com>
2024-11-27 13:25:05 -0500

    para presentar

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="css/estilos.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 </head>
 
@@ -24,17 +26,17 @@
             <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Servicios</a>
+            <a class="nav-link" href="#servicios">Servicios</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Nuestros Clientes</a>
+            <a class="nav-link" href="#clientes">Nuestros Clientes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contáctenos</a>
+            <a class="nav-link" href="#contacto">Contáctenos</a>
           </li>
         </ul>
         <span class="navbar-text">
-          40-26 82nd Street 2FL of # 2, Jackson Heights, NY 11373
+          8402 Roosevelt Avenue Bsmt Suite L14 Jackson Heights, NY 11372
         </span>
       </div>
     </div>
@@ -58,7 +60,9 @@
   <div class="one">
     <div class="row">
       <div class="col-md-6">
-        <img src="img/001.jpg" alt="one" width="100%">
+        <div data-aos="fade-right">
+          <img src=" img/003.jpg" alt="one" width="100%">
+        </div>
       </div>
       <div class="col-md-6 p-5">
         <h2> Servicios de inmigración </h2>
@@ -67,8 +71,7 @@
           Peticiones familiares.</br>
           Ajuste de Status.</br>
           Renovación residencia.</br>
-          Renovación permiso</br>
-          de trabajo.</br>
+          Renovación permiso de trabajo.</br>
           Vawa.</br>
           Visa U.</br>
           TPS.</br>
@@ -77,62 +80,132 @@
     </div>
   </div>
 
-  <div class="one">
+  <div class="one" id="servicios">
     <div class="row">
       <div class="col-md-6 p-5">
         <h2> Servicios de impuestos </h2>
         <hr>
         <p>
-          Tax services.</br>
+          Impuestos personales & corporativos.</br>
           ITIN / Tax ID Number.</br>
-          Tax preparer.</br>
         </p>
-        </br>
-        <h2> Servicios empresariales </h2>
+      </div>
+      <div class="col-md-6">
+        <div data-aos="fade-left">
+          <img src="img/002.jpg" alt="one" width="100%">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="one">
+    <div class="row">
+      <div class="col-md-6">
+        <div data-aos="fade-right">
+          <img src="img/001.jpg" alt="one" width="100%">
+        </div>
+      </div>
+      <div class="col-md-6 p-5">
+        <h2> Servicios empresariales</h2>
         <hr>
         <p>
-          Business formation Corp</br>
+          Business formation Corp.</br>
           Corporation, Bookkeeping & Payroll.</br>
           LLC & INC.</br>
         </p>
       </div>
-      <div class="col-md-6">
-        <img src="img/002.jpg" alt="one" width="100%">
+    </div>
+  </div>
+
+  <div class="row two my-5 p-5" id="clientes">
+    <div class="col-md-12">
+      <h2>Testimonios</h2>
+      <hr>
+    </div>
+    <div class="col-md-4 text-center">
+      <div data-aos="fade-up">
+        <div class="bg-white text-black p-5">
+          <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p class="firma text-end">- Patata -</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center">
+      <div data-aos="fade-up">
+        <div class="bg-white text-black p-5">
+          <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p class="firma text-end">- Patata -</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center">
+      <div data-aos="fade-up">
+        <div class="bg-white text-black p-5">
+          <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p class="firma text-end">- Patata -</p>
+        </div>
       </div>
     </div>
   </div>
 
-  <div class="row my-5 p-5">
-    <div class="col-md-4 text-center card p-3">
-      <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-      <p class="firma text-end">- Patata -</p>
-    </div>
-    <div class="col-md-4 text-center card p-3">
-      <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-      <p class="firma text-end">- Patata -</p>
-    </div>
-    <div class="col-md-4 text-center card p-3">
-      <img src="img/profile01.jpg" class="rounded-circle" alt="profile pic" width="90px">
-      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-      <p class="firma text-end">- Patata -</p>
+  <div class="one">
+    <div class="row">
+      <div class="col-md-6 p-5">
+        <h2> Servicios adicionales </h2>
+        <hr>
+        <p>
+          Oficiantes de bodas.</br>
+          Divorcio.</br>
+          Health & life Insurances.</br>
+        </p>
+      </div>
+      <div class="col-md-6">
+        <div data-aos="fade-left">
+          <img src="img/008.jpg" alt="one" width="100%">
+        </div>
+      </div>
     </div>
   </div>
 
-  <div class="two my-5">
+  <div class="two my-5" id="contacto">
     <div class="row p-5">
-      <div class="col-md-4 text-center">
+      <h2>¡Contacta con nuestro equipo!</h2>
+      <hr>
+      <div class="col-md-5 text-center">
+        <img src="img/009.jpg" alt="imagen de pareja" width="60%">
+      </div>
+      <div class="col-md-7">
+        <form action="index.php" method="post">
+          <label>Nombre</label>
+          <input class="form-control" type="text">
+          <label>Dirección electrónica</label>
+          <input class="form-control" type="text">
+          <label>Número celular</label>
+          <input class="form-control" type="text">
+          <label>Detalles</label>
+          <textarea class="form-control" type="text"></textarea>
+          <input class="form-control btn btn-success" type="text" value="Enviar">
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer p-5 text-center">
+    <div class="row">
+      <div class="col-md-4">
         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="img/INMI 1.jpg" alt="foto" width="90%">
+              <img src="img/006.jpg" alt="foto" width="90%">
             </div>
             <div class="carousel-item">
-              <img src="img/INMI 2.jpg" alt="foto" width="90%">
+              <img src="img/004.jpg" alt="foto" width="90%">
             </div>
             <div class="carousel-item">
-              <img src="img/INMI 3.jpg" alt="foto" width="90%">
+              <img src="img/005.jpg" alt="foto" width="90%">
             </div>
           </div>
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -144,52 +217,35 @@
             <span class="visually-hidden">Next</span>
           </button>
         </div>
-
       </div>
-      <div class="col-md-6">
-        <h2>¡Contacta con nuestrp equipo!</h2>
-        <form action="index.php" method="post">
-          <label>Nombre</label>
-          <input class="form-control" type="text">
-          <label>Dirección electrónica</label>
-          <input class="form-control" type="text">
-          <label>Número celular</label>
-          <input class="form-control" type="text">
-          <label>Número celular</label>
-          <textarea class="form-control" type="text"></textarea>
-          <input class="form-control btn btn-success" type="text" value="Enviar">
-        </form>
-      </div>
-    </div>
-  </div>
-
-  <footer class="footer p-5 text-center">
-    <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-4 mt-5">
         <p class="footer">
-          Contáctenos<br />
-          <hr>
           8402 Roosevelt Avenue Bsmt Suite L14<br />
           Jackson Heights, NY 11372<br />
           (929) 227 - 6534<br />
           (929) 320 - 7423<br />
           mfgenerals@gmail.com<br />
         </p>
-      </div>
-      <div class="col-md-4">
+        <hr>
         <p class="footer"> MF GENERAL SERVICES NYC </br>
           2024 | Todos los derechos reservados </p>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 text-center">
         <p class="footer">Redes Sociales</p>
+        <a href="https://www.instagram.com/mfgsnyc/" target="_blank"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.facebook.com/mf.general.services.corp" target="_blank"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.tiktok.com/@mfgsnyc" target="_blank"><i class="bi bi-tiktok"></i></a>
       </div>
     </div>
   </footer>
 
 </body>
 
-
-
+<script src="js/func.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
